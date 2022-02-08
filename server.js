745f178a2c39cb4e324bbf79b29d304e9bf1484a
Sidharth P <sidharthpunathil714@gmail.com>
2022-02-08 22:10:56 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+    res.render("pages/index");
+});
+
+app.get("/about", function (req, res) {
+    res.render("pages/about");
+});
+
+app.listen(8080);
+console.log("Server is listening on port 8080");
